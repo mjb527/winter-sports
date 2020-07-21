29405278339b9293195c091d14859c29b99ea67f
Matt <mbatko0914@gmail.com>
2020-07-21 18:39:57 -0400

    basic structure, App built out a bit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,35 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+
+// components
+import Header from './components/Header/header';
+import Nav from './components/Nav/nav';
+import Footer from './components/Footer/footer';
+import SportDir from './components/SportDir/sport-dir';
+import SportContent from '.components/SportContent/sport-content';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <Header />
+    <Nav />
+
+      <div className="row">
+
+        <div className="">
+          { /* left side, shows the sport and some of the options for it */ }
+          <SportDir />
+        </div>
+
+        <div className="">
+          { /* right side, shows the page content */ }
+          <SportContent />
+        </div>
+
+      </div>
+
+    <Footer />
+
     </div>
   );
 }
